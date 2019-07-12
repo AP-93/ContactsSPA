c6eaa835020de0ab3027079fa77cd82014b6f436
AP-93 <AP-93@users.noreply.github.com>
2019-07-12 15:34:02 +0200

    Initial commit

---
+++ src/app/ContactsList/contact.ts
@@ -0,0 +1,5 @@
+export interface IContact{
+    contactName: string;
+    contactEmail:string[];
+    contactPhoneNum:string;
+}
---
+++ src/app/ContactsList/contactsList.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { IContact } from './contact';
+import { ContactService } from './contact.service';
+
+@Component({
+  selector: 'app-contactsList',
+  templateUrl: './contactsList.component.html',
+  styleUrls: ['./contactsList.component.css']
+})
+export class ContactsListComponent implements OnInit {
+
+  _listFilter: string;
+  filteredContacts: IContact[];
+  contacts: IContact[];
+  private _contactService;
+  errorMsg:string;
+
+  constructor(contactService: ContactService) {
+    this._contactService = contactService;
+  }
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredContacts = this._listFilter ? this.filterByName(this._listFilter) : this.contacts;
+  }
+
+  filterByName(filterBy: string): IContact[] {
+    filterBy = filterBy.toLowerCase();
+    return this.contacts.filter((contact: IContact) => contact.contactName.toLowerCase().indexOf(filterBy) > -1)
+  }
+
+  ngOnInit(): void {
+    this._contactService.getContacts().subscribe(
+      x => {
+        this.contacts = x;
+        this.filteredContacts = this.contacts;
+      },
+      error => this.errorMsg = <any>error    
+      );
+
+  }
+
+}
